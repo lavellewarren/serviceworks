33ec50a39a411a3c13444e632cc110cac771577c
newcometlab <newcometlab@outlook.com>
2018-04-29 20:13:59 -0700

    added new employee view

--- src/pages/App.js
+++ src/pages/App.js
@@ -631,7 +631,7 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
 class NewCustomer extends Component {
   render() {
     return (
-      <div className="new-customer-view page-view">
+      <div className="new-customer-view person-view page-view">
         <div className="page-header">
           <h1>New Customer</h1>
         </div>
@@ -1297,6 +1297,325 @@ class TeamMap extends Component {
   }
 }
 
+class NewEmployee extends Component {
+  render() {
+    return (
+      <div className="new-employee-view person-view page-view">
+        <div className="page-header">
+          <h1>New Employee</h1>
+        </div>
+        <div className="page-body">
+          <div className="tab-btn-group">
+            <Link to="/my-account">
+              <button className="btn second-btn ">Cancel</button>
+            </Link>
+            <Link to="/my-account">
+              <button className="btn second-btn btn-success">Save Employee</button>
+            </Link>
+          </div>
+          <Tabs>
+            <TabList>
+              <Tab>Details</Tab>
+              <Tab>Jobs</Tab>
+              <Tab>Notes</Tab>
+              <Tab>Invoices</Tab>
+            </TabList>
+
+            <TabPanel>
+              <form className="person-form">
+                <div className="input-group">
+                  <label>
+                    Name and company
+                  </label>
+                  <div className="col-2">
+                    <div>
+                      <input type="text" placeholder="First name" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Last name" />
+                    </div>
+                  </div>
+                  <div className="col-2">
+                    <div>
+                      <input type="text" placeholder="Role" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Nickname" />
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label>
+                    Address
+                  </label>
+                  <div className="col-2 with-map">
+                    <div className="col-1">
+                      <input type="text" placeholder="Street address" />
+                      <input type="text" placeholder="Apt / suite / floor" />
+                      <input type="text" placeholder="City" />
+                      <input type="text" placeholder="State" />
+                      <div className="row col-80">
+                        <div>
+                          <select>
+                            <option>California</option>
+                            <option>New York</option>
+                          </select>
+                        </div>
+                        <div>
+                          <input type="text" placeholder="Zip code" />
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="map">
+                        <MyMapComponent
+                          isMarkerShown
+                          googleMapURL={googleMapUrl}
+                          loadingElement={<div style={{ height: `100%` }} />}
+                          containerElement={<div style={{ height: `100%` }} />}
+                          mapElement={<div style={{ height: `100%` }} />}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label>
+                    Contact
+                  </label>
+                  <div className="col-2">
+                    <div>
+                      <input type="text" placeholder="Mobile phone" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Home phone" />
+                    </div>
+                  </div>
+                  <div className="col-2">
+                    <div>
+                      <input type="text" placeholder="Email" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Office phone" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-header"> 
+                <Link to="/schedule/new-job">
+                  <span>+</span><h2>New Job</h2>
+                </Link>
+              </div>
+              <div className="tab-body">
+                <table className="panel">
+                  <thead>
+                    <tr className="header">
+                      <th><h2>Job name</h2></th>
+                      <th><h2>Date</h2></th>
+                      <th><h2>Time</h2></th>
+                      <th><h2>Location</h2></th>
+                    </tr>
+                  </thead>
+                  <tbody className="panel-body">
+                    <tr>
+                      <td><Link to="/schedule/new-job">Window cleaning</Link></td>
+                      <td>April 21, 2018</td>
+                      <td>1:00am - 2:00 am</td>
+                      <td>42 W 89th St, New York</td>
+                    </tr>
+                    <tr>
+                      <td><Link to="/schedule/new-job">Window cleaning</Link></td>
+                      <td>April 21, 2018</td>
+                      <td>1:00am - 2:00 am</td>
+                      <td>42 W 89th St, New York</td>
+                    </tr>
+                    <tr>
+                      <td><Link to="/schedule/new-job">Window cleaning</Link></td>
+                      <td>April 21, 2018</td>
+                      <td>1:00am - 2:00 am</td>
+                      <td>42 W 89th St, New York</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-header notes"> 
+                <Link to="/notes/new-note">
+                  <span>+</span><h2>New Note</h2>
+                </Link>
+              </div>
+              <div className="page-body">
+                <div className="notes-list panel">
+                  <div className="header">
+                    <h2>Note text</h2>
+                    <h2>Created</h2>
+                  </div>
+                  <div className="panel-body">
+                    <div className="sort-group">
+                      <div className="month-group"><span>March 2018</span></div>
+                      <div className="group-body">
+                        <div className="note">
+                          <div className="note-left">
+                            <div className="note-text">
+                              <div className="note-title">This is the first note</div>
+                              <div className="note-body">
+                                <p>This is the body text of the note. Im sure a lot better content will be written here</p>
+                              </div>
+                            </div>
+                            <div className="note-img">
+                              <img src={noteImg1} alt="note-img" />
+                            </div>
+                          </div>
+                          <div className="note-right">
+                            <div className="note-created">3/07/18</div>
+                          </div>
+                        </div>
+                        <div className="note">
+                          <div className="note-left">
+                            <div className="note-text">
+                              <div className="note-title">This is the first note</div>
+                              <div className="note-body">
+                                <p>This is the body text of the note. Im sure a lot better content will be written here</p>
+                              </div>
+                            </div>
+                            <div className="note-img">
+                              <img src={noteImg1} alt="note-img" />
+                            </div>
+                          </div>
+                          <div className="note-right">
+                            <div className="note-created">3/07/18</div>
+                          </div>
+                        </div>
+                        <div className="note">
+                          <div className="note-left">
+                            <div className="note-text">
+                              <div className="note-title">This is the first note</div>
+                              <div className="note-body">
+                                <p>This is the body text of the note. Im sure a lot better content will be written here</p>
+                              </div>
+                            </div>
+                            <div className="note-img">
+                              <img src={noteImg1} alt="note-img" />
+                            </div>
+                          </div>
+                          <div className="note-right">
+                            <div className="note-created">3/07/18</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="sort-group">
+                      <div className="month-group"><span>March 2018</span></div>
+                      <div className="group-body">
+                        <div className="note">
+                          <div className="note-left">
+                            <div className="note-text">
+                              <div className="note-title">This is the first note</div>
+                              <div className="note-body">
+                                <p>This is the body text of the note. Im sure a lot better content will be written here</p>
+                              </div>
+                            </div>
+                            <div className="note-img">
+                              <img src={noteImg1} alt="note-img" />
+                            </div>
+                          </div>
+                          <div className="note-right">
+                            <div className="note-created">3/07/18</div>
+                          </div>
+                        </div>
+                        <div className="note">
+                          <div className="note-left">
+                            <div className="note-text">
+                              <div className="note-title">This is the first note</div>
+                              <div className="note-body">
+                                <p>This is the body text of the note. Im sure a lot better content will be written here</p>
+                              </div>
+                            </div>
+                            <div className="note-img">
+                              <img src={noteImg1} alt="note-img" />
+                            </div>
+                          </div>
+                          <div className="note-right">
+                            <div className="note-created">3/07/18</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-header invoices"> 
+                <Link to="/invoices">
+                  <span>+</span><h2>New Invoice</h2>
+                </Link>
+              </div>
+              <div className="tab-body">
+                <table className="panel invoices">
+                  <thead>
+                    <tr className="header">
+                      <th><h2>Number</h2></th>
+                      <th><h2>Customer</h2></th>
+                      <th><h2>Title</h2></th>
+                      <th><h2>Date</h2></th>
+                      <th><h2>Amount due</h2></th>
+                      <th><h2>Status</h2></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody className="panel-body">
+                    <tr>
+                      <td><Link to="/invoices/new-invoice">0001</Link></td>
+                      <td><Link to="customers/new-customer">Sally May</Link></td>
+                      <td><Link to="/invoices/new-invoice">Window cleaning</Link></td>
+                      <td>4/28/18</td>
+                      <td>$240</td>
+                      <td>Current</td>
+                      <td><img src={threeDots} alt="item-menu" /></td>
+                    </tr>
+                    <tr>
+                      <td><Link to="/invoices/new-invoice">0001</Link></td>
+                      <td><Link to="customers/new-customer">Sally May</Link></td>
+                      <td><Link to="/invoices/new-invoice">Window cleaning</Link></td>
+                      <td>4/28/18</td>
+                      <td>$240</td>
+                      <td>Current</td>
+                      <td><img src={threeDots} alt="item-menu" /></td>
+                    </tr>
+                    <tr>
+                      <td><Link to="/invoices/new-invoice">0001</Link></td>
+                      <td><Link to="customers/new-customer">Sally May</Link></td>
+                      <td><Link to="/invoices/new-invoice">Window cleaning</Link></td>
+                      <td>4/28/18</td>
+                      <td>$240</td>
+                      <td>Current</td>
+                      <td><img src={threeDots} alt="item-menu" /></td>
+                    </tr>
+                    <tr>
+                      <td><Link to="/invoices/new-invoice">0001</Link></td>
+                      <td><Link to="customers/new-customer">Sally May</Link></td>
+                      <td><Link to="/invoices/new-invoice">Window cleaning</Link></td>
+                      <td>4/28/18</td>
+                      <td>$240</td>
+                      <td>Current</td>
+                      <td><img src={threeDots} alt="item-menu" /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </TabPanel>
+          </Tabs>
+        </div>
+
+      </div>
+    )
+  }
+}
+
 class MyAccount extends Component {
   render() {
     return (
@@ -1398,7 +1717,9 @@ class MyAccount extends Component {
             <TabPanel>
               <div className="tab-header">
                 <h2>BensWorth Cleaning Company</h2>
-                <button className="invoice-btn btn"><img src={plus} alt="" /><span>New employee</span></button>
+                <Link to="/my-account/new-employee">
+                  <button className="invoice-btn btn"><img src={plus} alt="" /><span>New employee</span></button>
+                </Link>
               </div>
               <div className="employee-list-wrapper">
                 <table className="panel">
@@ -1446,11 +1767,12 @@ class App extends Component {
           <Route path="/customers" exact component={Customers} />
           <Route path="/invoices" exact component={Invoices} />
           <Route path="/team-map" component={TeamMap} />
-          <Route path="/my-account" component={MyAccount} />
+          <Route path="/my-account" exact component={MyAccount} />
           <Route exact path="/schedule/new-job" component={NewJob} />
           <Route exact path="/notes/new-note" component={NewNote} />
           <Route exact path="/customers/new-customer" component={NewCustomer} />
           <Route exact path="/invoices/new-invoice" component={NewInvoice} />
+          <Route exact path="/my-account/new-employee" component={NewEmployee} />
         </div>
       </Router>
     );

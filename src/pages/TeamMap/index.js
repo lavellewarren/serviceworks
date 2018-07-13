f19267069a12a7c0bbdaa664f30ccdaaef6015f6
newcometlab <newcometlab@outlook.com>
2018-07-13 05:26:22 -0700

    can show and add customer on map

--- src/pages/TeamMap/index.js
+++ src/pages/TeamMap/index.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react'
-import MultiMarkerMap  from '../../components/MultiMarkerMap'
+import MultiMarkerMap from '../../components/MultiMarkerMap'
 import { getCustomers } from '../../actions'
 import { connect } from 'react-redux';
 
+class CustomerList extends Component {
+   state = {
+    customers: this.props.customers
+  };
+  componentDidUpdate(prevProps) {
+    //adding customers to state
+    if (this.props.customers !== prevProps.customers) {
+      //Is this a anti pattern?
+      this.setState({customers: this.props.customers});
+    }
+  }
+  render() {
+    const customers = this.state.customers;
+    
+    const customerList = customers.map((customer, i) => {
+      return (
+        <div className="customer" key={i}>
+          <label><input type="checkbox" name={i}  onChange={this.props.onCustomerChange} checked={customer.displayOnMap}/><span>{customer.name}</span></label>
+        </div>
+      )
+    })
+    return customerList;
+  }
+}
+
 class TeamMapComp extends Component {
+  state = {
+    customers: this.props.customers
+  };
+
+  componentDidUpdate(prevProps) {
+    //adding customers to state
+    if (this.props.customers !== prevProps.customers) {
+      //Is this a anti pattern?
+      const customers = this.props.customers.map((customer) => {
+        customer.displayOnMap = true;
+        return customer;
+      })
+      this.setState({customers: customers});
+    }
+  }
   componentWillMount() {
     this.props.getCustomers();
   }
+  onCustomerChange = (e) => {
+    const target = e.target;
+    const value = target.checked;
+    const idx = target.name;
+    const customers = this.state.customers;
+    customers[idx].displayOnMap = value;
+
+    this.setState({customers});
+
+  }
   render() {
-    const customers = this.props.customers;
+    const customers = this.state.customers;
+    const filterdCustomers = customers.filter((customer)=> {
+        return customer.displayOnMap === true 
+    });
+
     return (
       <div className="map-view page-view">
         <div className="map">
-          <MultiMarkerMap latLng={{ lat: 37.8145218, lng: -122.2544248 }} items={customers} />
+          <MultiMarkerMap latLng={{ lat: 37.8145218, lng: -122.2544248 }} items={filterdCustomers} />
         </div>
         <aside className="side-area">
           <div className="side-header">
@@ -20,18 +74,7 @@ class TeamMapComp extends Component {
             <hr />
           </div>
           <div className="side-body">
-            <div className="customer">
-              <label><input type="checkbox" /><span>All Customers</span></label>
-            </div>
-            <div className="customer">
-              <label><input type="checkbox" /><span>Jim Jones</span></label>
-            </div>
-            <div className="customer">
-              <label><input type="checkbox" /><span>Lilly Mack</span></label>
-            </div>
-            <div className="customer">
-              <label><input type="checkbox" /><span>Sammy Brown</span></label>
-            </div>
+            <CustomerList customers={customers} onCustomerChange={this.onCustomerChange}/>
             <button className="btn second-btn btn-success">Add Customer</button>
           </div>
         </aside>
@@ -44,5 +87,5 @@ class TeamMapComp extends Component {
 const mapNoteStateToProps = state => ({
   customers: state.customers.customers
 });
-export const TeamMap = connect(mapNoteStateToProps, {getCustomers})(TeamMapComp);
+export const TeamMap = connect(mapNoteStateToProps, { getCustomers })(TeamMapComp);
 

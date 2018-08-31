734261fbcd99be4bd6190b2dd609c3e5013263d8
newcometlab <newcometlab@outlook.com>
2018-08-31 07:17:37 -0700

    working page navs

--- src/components/PageNavs.js
+++ src/components/PageNavs.js
@@ -23,25 +23,27 @@ export class PageNavs extends Component {
     return (
       <div>
         <Modal
+          className="confrimation-modal"
           isOpen={this.props.openExitModal}
           onRequestClose={() => this.props.handleCloseModal()}
           shouldCloseOnOverlayClick={true}
         >
           <h3>You have unsaved changes</h3>
           <div className="button-wrapper">
-            <button onClick={(e) => this.props.onSave(this.props.payload, e)}>save & close</button>
-            <button onClick={() => this.props.handleCloseModal()}>cancel</button>
+            <button className="btn second-btn btn-success" onClick={(e) => this.props.onSave(this.props.payload, e)}>save & close</button>
+            <button className="btn second-btn btn-cancel" onClick={() => this.props.handleCloseModal()}>cancel</button>
           </div>
         </Modal>
         <Modal
+          className="confrimation-modal"
           isOpen={this.props.openDeleteModal}
           onRequestClose={() => this.props.handleCloseModal()}
           shouldCloseOnOverlayClick={true}
         >
           <h3>Are you sure you want to delete?</h3>
           <div className="button-wrapper">
-            <button onClick={(e) => this.props.onDelete (this.props.payload.id, e)}>delete & close</button>
-            <button onClick={() => this.props.handleCloseModal()}>cancel</button>
+            <button className="btn second-btn btn-delete" onClick={(e) => this.props.onDelete (this.props.payload.id, e)}>Delete & close</button>
+            <button className="btn second-btn btn-cancel " onClick={() => this.props.handleCloseModal()}>Cancel</button>
           </div>
         </Modal>
         <div className="tab-btn-group">
